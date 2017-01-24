f6b041744d558d1a47328d368070684fa4ccb17f
Björn Ritzl <bjorn.ritzl@king.com>
2017-01-24 11:30:53 +0100

    Initial commit of assets and code

---
+++ main/stoneblock_round.go
@@ -0,0 +1,102 @@
+components {
+  id: "script"
+  component: "/main/block.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  properties {
+    id: "durability"
+    value: "100.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "undamaged"
+    value: "elementStone001"
+    type: PROPERTY_TYPE_HASH
+  }
+  properties {
+    id: "damaged"
+    value: "elementStone007"
+    type: PROPERTY_TYPE_HASH
+  }
+  properties {
+    id: "almost_destroyed"
+    value: "elementStone045"
+    type: PROPERTY_TYPE_HASH
+  }
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
+  "mass: 1000.0\n"
+  "friction: 1.0\n"
+  "restitution: 0.2\n"
+  "group: \"block\"\n"
+  "mask: \"block\"\n"
+  "mask: \"bird\"\n"
+  "mask: \"ground\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_SPHERE\n"
+  "    position {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 1\n"
+  "  }\n"
+  "  data: 35.0\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/main/angrybirds.atlas\"\n"
+  "default_animation: \"elementStone001\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
---
+++ main/woodenblock_square.go
@@ -0,0 +1,104 @@
+components {
+  id: "script"
+  component: "/main/block.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  properties {
+    id: "durability"
+    value: "25.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "undamaged"
+    value: "elementWood012"
+    type: PROPERTY_TYPE_HASH
+  }
+  properties {
+    id: "damaged"
+    value: "elementWood015"
+    type: PROPERTY_TYPE_HASH
+  }
+  properties {
+    id: "almost_destroyed"
+    value: "elementWood047"
+    type: PROPERTY_TYPE_HASH
+  }
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
+  "mass: 200.0\n"
+  "friction: 1.0\n"
+  "restitution: 0.2\n"
+  "group: \"block\"\n"
+  "mask: \"block\"\n"
+  "mask: \"bird\"\n"
+  "mask: \"ground\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 110.0\n"
+  "  data: 35.0\n"
+  "  data: 10.0\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/main/angrybirds.atlas\"\n"
+  "default_animation: \"elementWood012\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
