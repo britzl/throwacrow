966ed61d3fbc43903ce3a38100e65260022f1886
Björn Ritzl <bjorn.ritzl@king.com>
2017-01-26 22:04:33 +0100

    Added non functioning explosives crate

---
+++ main/explosives.go
@@ -0,0 +1,129 @@
+components {
+  id: "script"
+  component: "/main/block.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  properties {
+    id: "durability"
+    value: "250.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "undamaged"
+    value: "elementExplosive011"
+    type: PROPERTY_TYPE_HASH
+  }
+  properties {
+    id: "damaged"
+    value: "elementExplosive014"
+    type: PROPERTY_TYPE_HASH
+  }
+  properties {
+    id: "almost_destroyed"
+    value: "elementExplosive049"
+    type: PROPERTY_TYPE_HASH
+  }
+  properties {
+    id: "debris"
+    value: "false"
+    type: PROPERTY_TYPE_BOOLEAN
+  }
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
+  "mass: 500.0\n"
+  "friction: 0.9\n"
+  "restitution: 0.2\n"
+  "group: \"block\"\n"
+  "mask: \"block\"\n"
+  "mask: \"bird\"\n"
+  "mask: \"ground\"\n"
+  "mask: \"wind\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 3\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 32.0\n"
+  "  data: 34.5\n"
+  "  data: 10.0\n"
+  "  data: 34.5\n"
+  "  data: 32.0\n"
+  "  data: 10.0\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/main/angrybirds.atlas\"\n"
+  "default_animation: \"elementExplosive011\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
